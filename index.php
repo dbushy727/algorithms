3a12fd594bde6f2a277c8c9b67cdea718a3d4fda
dbushy727 <d.bushkanets@gmail.com>
2016-03-01 00:26:36 -0500

    took out behavioral tests from index.php

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
 <?php
 
 require 'vendor/autoload.php';
-
-use Algorithm\Search\BinarySearch;
-use Algorithm\Search\LinearSearch;
-
-$input         = range(0, 500);
-$target        = 30;
-
-$linear_search = (new LinearSearch)->search($input, $target);
-$binary_search = (new BinarySearch)->search($input, $target);
-
-var_dump(compact('linear_search', 'binary_search'));

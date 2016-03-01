ff300aacd86af7bd58dfbda4e80b1cf3c9429fb4
dbushy727 <d.bushkanets@gmail.com>
2016-03-01 00:02:41 -0500

    took out unnecessary assignments in index.php

--- index.php
+++ index.php
@@ -5,13 +5,10 @@ require 'vendor/autoload.php';
 use Algorithm\Search\BinarySearch;
 use Algorithm\Search\LinearSearch;
 
-$linear_search = new LinearSearch;
-$binary_search = new BinarySearch;
 $input         = range(0, 500);
 $target        = 30;
 
+$linear_search = (new LinearSearch)->search($input, $target);
+$binary_search = (new BinarySearch)->search($input, $target);
 
-$linear_output = $linear_search->search($input, $target);
-$binary_output = $binary_search->search($input, $target);
-
-var_dump(compact('linear_output', 'binary_output'));
+var_dump(compact('linear_search', 'binary_search'));
